cce0727ccb12f3bb867391cbab84fec9a19d96b5
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2022-10-09 17:03:13 +0700

    fixing bugs

--- src/service/PlaylistsService.js
+++ src/service/PlaylistsService.js
@@ -6,14 +6,23 @@ module.exports = class PlaylistsService {
   }
 
   async getPlaylists(userId) {
-    const res = await this._pool.query({
-      text: `SELECT p.id, p.name, u.username FROM playlists p
-            LEFT JOIN collaborations c on c.playlist_id = p.id
-            LEFT JOIN users u ON u.id = p.owner
-             WHERE p.owner = $1 OR c.user_id = $1`,
+    const playlist = await this._pool.query({
+      text: `SELECT p.id, p.name FROM playlists p
+            LEFT JOIN collaborations c ON c.playlist_id = p.id
+            WHERE p.owner = $1 OR c.user_id = $1`,
       values: [userId],
     });
 
-    return res.rows;
+    const songs = await this._pool.query({
+      text: `SELECT s.id, s.title, s.performer FROM songs s
+            LEFT JOIN playlist_songs ps ON s.id = ps.song_id
+            LEFT JOIN playlists p ON p.id = ps.playlist_id
+            WHERE p.owner = $1`,
+      values: [userId],
+    });
+
+    playlist.rows[0].songs = songs.rows;
+
+    return playlist.rows[0];
   }
 };
